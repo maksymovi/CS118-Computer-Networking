a9e88d27fd0272cf11e918a687e0916219dfbac6
Maksym Prokopovych <24415429+maksymovi@users.noreply.github.com>
2021-04-23 08:18:19 -0700

    Testing socket operation

--- 1/main.c
+++ 1/main.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
+#include <stdio.h>
 
 #include "auxiliary.h"
 
 
 //DEFINES
 #define MAIN_PORT 8001
-
+#define SOCKET_BACKLOG 5
 
 
 
@@ -19,8 +19,16 @@ int main()
 	//start by opening a socket of course.
 
 	int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	int acceptedfd;
 
+	FILE * acceptStream;
 	struct sockaddr_in addr;
+
+	char buffer[1000];
+	char* bufptr;
+	//things for accepted sockets
+	struct sockaddr_in acceptedAddr;
+	socklen_t acceptedAddrLen; //probably unnecessary but eh
 	
 	if(socketfd < 0) //fatal error, print and exit
 		fatalError("Socket creation error");
@@ -39,6 +47,30 @@ int main()
 	if(bind(socketfd, (struct sockaddr *) &addr, sizeof(addr))) //nonzero return is fatal error
 		fatalError("Socket Bind Error");
 
+	//listen
+	if(listen(socketfd, SOCKET_BACKLOG))
+		fatalError("Socket Listen Error");
+
+	//accept
+	acceptedfd = accept(socketfd, (struct sockaddr *) &acceptedAddr, &acceptedAddrLen);
+
+	if(acceptedfd < 0)
+		fatalError("Socket Accept Error");
+
+	acceptStream = fdopen(acceptedfd, "r");
+	
+	while((bufptr = fgets(buffer, 1000, acceptStream)) != NULL)
+	{
+		printf("%s", buffer);
+	}
+	printf("\n");
+	
+	if(close(acceptedfd))
+		fatalError("Socket Close Error");
+	
+	
+
+	
 	if(close(socketfd))
 		fatalError("Socket Close Error");
 	
