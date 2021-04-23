246258aafd7515b6cdb69871929e473ff9c63dc3
Maksym Prokopovych <24415429+maksymovi@users.noreply.github.com>
2021-04-23 12:30:19 -0700

    Wasted 6 hours because I used htonl instead of htons for the port number, but I at have managed to open a socket at least so now the http parsing begins.

--- 1/main.c
+++ 1/main.c
@@ -4,13 +4,15 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
 
 #include "auxiliary.h"
 
 
 //DEFINES
-#define MAIN_PORT 8101
-#define SOCKET_BACKLOG 5
+#define ENTRYPORT 8080
+#define SOCKET_BACKLOG 3
 
 
 
@@ -20,53 +22,49 @@ int main()
 
 	int socketfd = socket(AF_INET, SOCK_STREAM, 0);
 	int acceptedfd;
+	int count;
 
-	FILE * acceptStream;
 	struct sockaddr_in addr;
-
-	char buffer[1000];
-	char* bufptr;
-	//things for accepted sockets
-	struct sockaddr_in acceptedAddr;
-	socklen_t acceptedAddrLen; //probably unnecessary but eh
 	
-	if(socketfd < 0) //fatal error, print and exit
+	char buffer[1000];
+		
+	socklen_t addrLen = sizeof(addr); //probably unnecessary but eh
+	
+	if(socketfd <= 0) //fatal error, print and exit
 		fatalError("Socket creation error");
 
 	//possible use of setsockopt here, but not needed for now
 
+
 	//setting up for bind
 	addr.sin_family = AF_INET;
-	addr.sin_port = htonl(MAIN_PORT);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons( ENTRYPORT );
+	addr.sin_addr.s_addr = INADDR_ANY;
+
 	//and might as well set the 0 bytes
+	
 	for(int i = 0; i < 8; i++)
 		addr.sin_zero[i] = 0;
-
+	
 	//now we bind
-	if(bind(socketfd, (struct sockaddr *) &addr, sizeof(addr))) //nonzero return is fatal error
+	if(bind(socketfd, (struct sockaddr *) &addr, sizeof(addr)) < 0 ) //nonzero return is fatal error
 		fatalError("Socket Bind Error");
 
 	//listen
-	if(listen(socketfd, SOCKET_BACKLOG))
+	if(listen(socketfd, SOCKET_BACKLOG) < 0)
 		fatalError("Socket Listen Error");
 
 	//accept
-	acceptedfd = accept(socketfd, (struct sockaddr *) &acceptedAddr, &acceptedAddrLen);
 
-	if(acceptedfd < 0)
+	if((acceptedfd = accept(socketfd, (struct sockaddr *) &addr, &addrLen)) < 0)
 		fatalError("Socket Accept Error");
 
-	//acceptStream = fdopen(acceptedfd, "r");
-	
-	int pipefds[2] = {acceptedfd, STDOUT_FILENO};
-
-	if(pipe(pipefds))
-		fatalError("Pipe Error");
-
-
-
 		
+	count = read(acceptedfd, buffer, 999);
+	if(count < 0)
+		fatalError("Read error");
+	buffer[999] = '\0';
+	printf("%s", buffer);
 	
 	if(close(acceptedfd))
 		fatalError("Socket Close Error");
